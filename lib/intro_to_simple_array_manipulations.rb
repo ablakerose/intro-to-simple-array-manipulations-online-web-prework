52bc10d774c7fc877ebbca78539f7eb6f790bbf1
Blake Rose <bdecktor@gmail.com>
2019-05-04 23:40:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,3 +2,8 @@ def using_push(array, next_country)
   next_country = ("Niger")
   array.push(next_country)
 end
+
+def using_unshift(array, updated_array)
+  new_neighborhood = "Brooklyn Heights"
+  array.unshift(new_neighborhood)= updated_array
+end 
