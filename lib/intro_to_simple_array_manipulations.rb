fbb9d3f9208db3479e1b98d08ccd135306945742
Blake Rose <bdecktor@gmail.com>
2019-05-04 23:37:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,4 @@
 def using_push(countries_in_western_africa, "Niger")
-  countries_in_western_africa.push("Niger")
+  next_country = ("Niger")
+  countries_in_western_africa.push(next_country) 
 end 
